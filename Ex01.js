57dcc69322b625485269855e4a1cbea2a35c85f6
Elaine Ferreira Cerqueira <elainecerqueira@conder.intranet>
2022-08-23 13:48:51 -0300

    execução de tarefa

---
+++ Ex01.js
@@ -0,0 +1,39 @@
+// ler o ano de nascimento de uma pessoa, calcular e mostrar sua idade e, também, verificar e mostrar se ela já tem idade para votar (16 anos ou mais)e para conseguir a Carteira de Habilitação
+
+// let anoAtual = 2022;
+// let anoNascimento = 2010;
+
+
+// let suaIdade = anoAtual - anoNascimento;
+
+// if(suaIdade){
+//     console.log("minha idade é" + suaIdade)
+// } if(suaIdade>16) {
+//     console.log  ("Apto a votar")
+// } else {
+//     console.log ("Menor de idade, não apto a votar");
+// }
+
+
+
+
+
+// let dataEvento = new Date(2025,08,15);
+
+// let dataAtual = new Date();
+
+// if(dataEvento>dataAtual){
+//     console.log("O evento será dia" + dataEvento)
+// } else {
+//     console.log("cadastro não será permitido por data inválida");
+// }
+
+
+let quantidade = 0;
+
+while (quantidade < 99) {
+   console.log("Realizar cadastro")
+  
+};
+
+
